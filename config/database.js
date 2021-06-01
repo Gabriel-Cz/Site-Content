1ebba6cb9dbb91a22b17008de98dd4406567d0cb
Gabriel Cazares <gabrielczhz@gmail.com>
2021-05-31 19:03:05 -0500

    resume edited

--- config/database.js
+++ config/database.js
@@ -4,10 +4,10 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        uri: env('DATABASE_URI'),
+        uri: env('DATABASE_URI' || 'mongodb+srv://Gabriel:Gabrieladmin123@cluster0.z3gjy.mongodb.net/portfolio-content?retryWrites=true&w=majority'),
         srv: env.bool('DATABASE_SRV', true),
         port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME'),
+        database: env('DATABASE_NAME' || 'portfolio-content'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
